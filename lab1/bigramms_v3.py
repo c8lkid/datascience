5166df4f53786a15d0a91b91a3a532d535cc62b7
a.gvozdev <a.gvozdev@rambler-co.ru>
2017-03-09 01:30:47 +0300

    edit v3

--- lab1/bigramms_v3.py
+++ lab1/bigramms_v3.py
@@ -2,9 +2,25 @@
 
 import sys
 
-bigramm = {}
+bigramms = {}
 
 for line in sys.stdin:
-    query = line.split('\t')[1]
-    if len(query.split(' ')) > 1:
 
+    query = line.split('\t')[1].split(' ')
+
+    if len(query) > 1:
+        bigramm_fst = '{0} {1}'.format(' '.join(query[:2]), ' '.join(query[3:]))
+        if bigramms.has_key(bigramm_fst):
+            bigramms[bigramm_fst] += 1
+        else:
+            bigramms[bigramm_fst] = 0
+
+    if len(query[1:]) > 1:
+        bigramm_snd = '{0}'.format(' '.join(query[1:]))
+        if bigramms.has_key(bigramm_snd):
+            bigramms[bigramm_snd] += 1
+        else:
+            bigramms[bigramm_snd] = 0
+
+for k, v in bigramms.items():
+    print('{1:>3} {0}'.format(k, v))
